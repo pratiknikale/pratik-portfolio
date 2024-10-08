b1683e873ecf26eae6bad44c41293605bfa1c327
pratik nikale <prnikale@gmail.com>
2024-10-08 11:07:10 +0530

    custom button importing issue fixed - build fail repair

--- app/component/Contact.js
+++ app/component/Contact.js
@@ -1,9 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
-import Button from "../commonComponents/button";
+import Button from "../commonComponents/Button";
 
 export default function Contact() {
 
